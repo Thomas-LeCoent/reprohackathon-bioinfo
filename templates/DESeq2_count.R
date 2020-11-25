88f1911ae9e5638ae61b32b03ee7f12e161454d0
TatsuyaDaniel <52911805+TatsuyaDaniel@users.noreply.github.com>
2020-11-25 23:43:39 +0100

    Update DESeq2_count.R
    add options for saving plot as an output thanks to pdf(paste0(.....)) and dev.off()

--- templates/DESeq2_count.R
+++ templates/DESeq2_count.R
@@ -1,5 +1,16 @@
 #!/usr/bin/env Rscript
 rm(list=ls())
+
+
+#################### install packages ######################
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("EnhancedVolcano")
+
+###########################################################
+
 library("DESeq2")
 #order columns
 col_order=c("Geneid", "SRR628582.bam", "SRR628583.bam", "SRR628584.bam", "SRR628585.bam", "SRR628586.bam", "SRR628587.bam", "SRR628588.bam", "SRR628589.bam")
@@ -19,6 +30,18 @@ dds <- DESeq(dds)
 res <- results(dds, name="mutation_WT_vs_R625C", alpha = 0.05)
 
 #table(res\$padj<0.05)[2]
+
+
+library("EnhancedVolcano")
+pdf(paste0("volcano.pdf"))
+
+
+EnhancedVolcano(res,
+                lab = rownames(res),
+                x = 'log2FoldChange',
+                y = 'pvalue')
+
+dev.off()
 
 nbgenes=length(res\$padj[which(res\$padj<0.05)])
 cat(nbgenes, file="results.txt")
